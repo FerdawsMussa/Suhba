83ca7239a3340c1ec6148f1b64ac022b7c1ecb58
Ferdaws <ferdousmussa16@gmail.com>
2026-01-29 15:20:34 +0300

    Delete main directory

--- main/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".sidebar li").forEach((item) => {
-  item.addEventListener("click", () => {
-    document.querySelector(".sidebar .active")?.classList.remove("active");
-    item.classList.add("active");
-  });
-});
